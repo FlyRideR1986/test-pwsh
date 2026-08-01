86a48943b5e1504f083c38b002fe5184d4bd08a9
FlyRideR1986 <31609725+FlyRideR1986@users.noreply.github.com>
2026-08-01 15:40:43 +0800

    Remove 'http2' protocol from cloudflared commands

--- koyeb/start_v2.sh
+++ koyeb/start_v2.sh
@@ -770,7 +770,7 @@ cat <<EOF > /etc/crontabs/root
 
 */5 * * * pgrep -x "/usr/local/bin/xray" > /dev/null || nohup /usr/local/bin/xray run -confdir /etc/xray/confs/ > /dev/null 2>&1 &
 
-*/5 * * * pgrep -x "/usr/local/bin/cloudflared" > /dev/null || nohup /usr/local/bin/cloudflared tunnel --protocol http2 --loglevel fatal run --token $CLOUDFLARED_TOKEN > /dev/null 2>&1 &
+*/5 * * * pgrep -x "/usr/local/bin/cloudflared" > /dev/null || nohup /usr/local/bin/cloudflared tunnel --loglevel fatal run --token $CLOUDFLARED_TOKEN > /dev/null 2>&1 &
 
 */5 * * * pgrep -f "nginx: master process" > /dev/null || nginx -c /etc/nginx/nginx.conf
 
@@ -788,7 +788,7 @@ apk del jq unzip wireguard-tools
 apk cache clean
 
 echo "后台启动cloudflared"
-nohup /usr/local/bin/cloudflared tunnel --protocol http2 --loglevel fatal run --token "$CLOUDFLARED_TOKEN" > /dev/null 2>&1 &
+nohup /usr/local/bin/cloudflared tunnel --loglevel fatal run --token "$CLOUDFLARED_TOKEN" > /dev/null 2>&1 &
 
 echo "后台启动xray"
 nohup /usr/local/bin/xray run -confdir /etc/xray/confs/ > /dev/null 2>&1 &
